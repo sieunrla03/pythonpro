0ce3ecc3104b0dfb3f48adc92adb7de225357801
sieun <kse9832@naver.com>
2023-09-20 05:02:10 +0000

    pro2 file uproad

--- chap2/pro2.py
+++ chap2/pro2.py
@@ -1,6 +1,16 @@
-name = input("Hi, What's you name? ")
-age = input("And how are you? ")
-weight = input("Okay, last question. How many kg do you weigh? ")
+name = input("Hi, What's your name? ")
+age = int(input("And how are you? "))
+weigh = int(input("Okay, last question. How many pounds do you weigh? \n"))
+dog = age/7
+print("Did you know that you're", int(dog), "just in dog year?")
 
-dogyear = age/7
+sec = age*360*24*60*60
+print("But you're also over", int(sec), "seconds old\n")
 
+print("If a small child were trying to get your attention, your name would become : \n", name*5)
+
+moon = weigh*0.17
+print("\nDid youknow that on the moon you would weigh only", round(moon,1),"pounds?")
+
+sun = weigh*28
+print("But on the sun, you'd weigh",round(sun,1),"(but, ah...not for long)")
