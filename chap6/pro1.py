fa7cc8be7b5cd9b886e69bcb2ca4846aaae49a57
sieun <kse9832@naver.com>
2023-10-11 01:51:01 +0000

    pro1 upload

---
+++ chap6/pro1.py
@@ -0,0 +1,38 @@
+inventory = ["sword", "armor", "shield", "healing potion"]
+print('Your items : ',)
+for item in inventory:
+	print(item)
+print('\nPress the enter key to continue.\n You have ',len(inventory),'items in your possession.\n')
+print('Press the enter key to continue.')
+if 'healing potion' in inventory:
+    print('You will live to fight another day.\n')
+index =int(input('Enter th index number for an item in inventory : '))
+print('At index',index, 'is', inventory[index])
+begin = int(input('\nEnter the index number to begin a slice: '))
+end = int(input('Enter the index number to end the slice : '))
+print ("inventory[", begin, ":", end, "] is:\t", inventory[begin : end], end = "")
+print('\n\nPress the enter key to continue.')
+chest = ['gold', 'gems']
+print('You find a chest.\t It contains : ', '<', chest[0:2],'>')
+inventory += chest
+print('\nYou add the contents of the chest to your inventory.\nYour inventory is now : \n',inventory)
+
+input('\nPress the enter key to continue.')
+print('You trade your sword for a crossbow.')
+inventory[0] = "crossbow"
+print('Your inventory is now: \n', inventory)
+
+input('\nPress the enter key to continue.')
+print('You use your gold and gems to buy an orb of future telling.')
+inventory[4:6] = ["orb of future telling"]
+print('Your inventory is now : \n', inventory)
+
+input('\nPress the enter key to continue.')
+print('You a great battle, your shield is destroyed.')
+del inventory[2]
+print('Your inventory is now : \n', inventory)
+
+input('\nPress the enter key to continue.')
+print('Your crossbow and armor are stolen by thieves.')
+del inventory[:2]
+print('Your inventory is now : \n', inventory)
