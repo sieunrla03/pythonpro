ae9a55fcf54351b58467084e3a2e07549d091bc4
sieun <kse9832@naver.com>
2023-12-13 01:43:22 +0000

    final

---
+++ final/prob1.py
@@ -0,0 +1,12 @@
+def Frequency_analytic(s):
+    alpha = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0,
+        'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0,
+        'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0,
+        'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0,
+        'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
+    msg = input("input your message : ")
+    for i in msg:
+        alpha[i] +=1
+    for s in alpha.values():
+        print(s, end="/n")
+print(Frequency_analytic(s))
